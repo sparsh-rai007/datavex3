1336f6b47254d9f8e50832a91dad7517cffdcb06
sparsh-rai007 <139550015+sparsh-rai007@users.noreply.github.com>
2025-12-13 18:02:02 +0530

    Set dynamic export to 'force-dynamic' in AssessmentPage

--- frontend/app/(public)/contact/assessment/AssessmentPage.tsx
+++ frontend/app/(public)/contact/assessment/AssessmentPage.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = "force-dynamic";
 import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { apiClient } from '@/lib/api';
